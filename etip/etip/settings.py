57aeb59acb4186cd98aca226974c81c1f87b4a6a
U+039b <git@0x39b.fr>
2018-04-01 19:24:15 +0200

    Initial commit

--- etip/etip/settings.py
+++ etip/etip/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'trackers',
     'reversion',
     'bootstrap3',
-    'trackers.apps.TrackersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ etip/trackers/management/commands/import_trackers.py
@@ -0,0 +1,48 @@
+import json
+
+from django.core.management.base import BaseCommand, CommandError
+
+from trackers.models import Tracker
+
+
+class Command(BaseCommand):
+    help = 'Import trackers from exodus'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'filename',
+            nargs = '?',
+            default = 'https://reports.exodus-privacy.eu.org/api/trackers'
+        )
+
+    def handle(self, *args, **options):
+        if Tracker.objects.all()[:1].count() > 0:
+            raise CommandError('Your trackers table in not empty, please truncate its before the import')
+
+        json_str = self.read_file(options['filename']).decode('utf-8')
+        trackers = json.loads(json_str)
+
+        for id, tracker in trackers['trackers'].items():
+            model = Tracker(
+                name = tracker['name'],
+                description = tracker['description'],
+                created = tracker['creation_date'],
+                code_signature = tracker['code_signature'],
+                network_signature = tracker['network_signature'],
+                website = tracker['website'],
+                is_in_exodus = True,
+            )
+
+            model.save()
+
+            print('%s saved' % model.name)
+
+    def read_file(self, filename):
+        if filename.find('://') > 0:
+            import urllib.request
+
+            file = urllib.request.urlopen(filename)
+        else:
+            file = open(filename, 'r')
+
+        return file.read()
