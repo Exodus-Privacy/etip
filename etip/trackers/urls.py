4bda81f7622ea2274aa3309585b41864f66f6e49
sim_pnu <simon@pruneau.info>
2019-01-31 13:40:13 +0100

    PEP8'd most of the code base

--- etip/trackers/urls.py
+++ etip/trackers/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'trackers'
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
+    url(r'^$', views.index, name='index'),
     # url(r'^(?P<tracker_id>[0-9]+)/$', views.detail, name = 'detail'),
 ]
