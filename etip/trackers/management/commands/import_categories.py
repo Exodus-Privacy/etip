466339bd240b4200ca589d40a9b53b51f432530e
sim_pnu <simon@pruneau.info>
2019-02-13 22:34:28 +0100

    refactor categories import

--- etip/trackers/management/commands/import_categories.py
+++ etip/trackers/management/commands/import_categories.py
@@ -7,95 +7,70 @@ class Command(BaseCommand):
     help = 'Import trackers categories'
 
     def handle(self, *args, **options):
-        m = Capability(name='Tracks users using bluetooth')
-        m.save()
-        m = Capability(name='Tracks users using ultrasonic')
-        m.save()
-        m = Capability(name='Tracks users using location data')
-        m.save()
-        m = Capability(name='Tracks users using GPS')
-        m.save()
-        m = Capability(name='Tracks users using WiFi')
-        m.save()
-        m = Capability(name='Tracks users using NFC')
-        m.save()
-        m = Capability(
-            name='Targets user location and proximity via geofencing')
-        m.save()
-        m = Capability(name='Targets users via geotargeting')
-        m.save()
+        capability_names = [
+            'Tracks users using bluetooth',
+            'Tracks users using ultrasonic',
+            'Tracks users using location data',
+            'Tracks users using GPS',
+            'Tracks users using WiFi',
+            'Tracks users using NFC',
+            'Targets user location and proximity via geofencing',
+            'Targets users via geotargeting',
+        ]
 
-        m = Advertising(name='Loads advertisements')
-        m.save()
-        m = Advertising(name='Loads targeted advertisements')
-        m.save()
-        m = Advertising(name='Real-world location targeting')
-        m.save()
-        m = Advertising(name='Targeted advertising based on consumer actions')
-        m.save()
-        m = Advertising(name='Timed advertisements')
-        m.save()
-        m = Advertising(
-            name='Targets across devices, channels and/or platforms \
-            (omni-channel marketing, customer journey)')
-        m.save()
-        m = Advertising(name='Bidding services')
-        m.save()
-        m = Advertising(name='Location-based ad pushing')
-        m.save()
-        m = Advertising(
-            name='Alters app functionality based upon user profiles')
-        m.save()
+        for name in capability_names:
+            m = Capability(name=name)
+            m.save()
 
-        m = Analytic(name='Offers analytics activity to app developers')
-        m.save()
-        m = Analytic(name='Offers reports to app developers')
-        m.save()
-        m = Analytic(name='Collects Personally Identifiable Information (PII)')
-        m.save()
-        m = Analytic(name='Collects Sensitive Personal Information (SPI)')
-        m.save()
-        m = Analytic(
-            name='Profiles users via Personally Identifiable Information \
-            (PII)')
-        m.save()
-        m = Analytic(
-            name='Profiles users via Sensitive Personal Information (SPI)')
-        m.save()
-        m = Analytic(name='Performs cross-device identification')
-        m.save()
-        m = Analytic(name='Identifies users via Google ID (AAID)')
-        m.save()
-        m = Analytic(name='Identifies users via iOS ID (IDFA)')
-        m.save()
-        m = Analytic(
-            name='Identifies users via network ID (hostname/ISP/SSID)')
-        m.save()
-        m = Analytic(name='Stores facial recognition data')
-        m.save()
-        m = Analytic(
-            name='Stores personal profile data (name, address, phone)')
-        m.save()
-        m = Analytic(name='Analytics AI and machine learning')
-        m.save()
-        m = Analytic(name='Audience segmenting')
-        m.save()
+        advertising_names = [
+            'Loads advertisements',
+            'Loads targeted advertisements',
+            'Real-world location targeting',
+            'Targeted advertising based on consumer actions',
+            'Timed advertisements',
+            'Targets across devices, channels and/or platforms (omni-channel marketing, customer journey)',
+            'Bidding services',
+            'Location-based ad pushing',
+            'Alters app functionality based upon user profiles',
+        ]
 
-        m = Network(name='Transmits user data to multiple ad networks')
-        m.save()
-        m = Network(name='Transmits information to Facebook ad network')
-        m.save()
-        m = Network(name='Transmits information to Google ad network')
-        m.save()
-        m = Network(name='Transmits information to Adobe ad network')
-        m.save()
-        m = Network(name='Transmits information to Yahoo! ad network')
-        m.save()
-        m = Network(name='Transmits information to Salesforce platform')
-        m.save()
-        m = Network(name='Transmits information to Twitter platform')
-        m.save()
-        m = Network(name='Transmits information to Amazon ad network')
-        m.save()
-        m = Network(name='Transmits information to Microsoft ad network')
-        m.save()
+        for name in advertising_names:
+            m = Advertising(name=name)
+            m.save()
+
+        analytic_names = [
+            'Offers analytics activity to app developers',
+            'Offers reports to app developers',
+            'Collects Personally Identifiable Information (PII)',
+            'Collects Sensitive Personal Information (SPI)',
+            'Profiles users via Personally Identifiable Information (PII)',
+            'Profiles users via Sensitive Personal Information (SPI)',
+            'Performs cross-device identification',
+            'Identifies users via Google ID (AAID)',
+            'Identifies users via iOS ID (IDFA)',
+            'Identifies users via network ID (hostname/ISP/SSID)',
+            'Stores facial recognition data',
+            'Stores personal profile data (name, address, phone)',
+            'Analytics AI and machine learning',
+            'Audience segmenting',
+        ]
+
+        for name in analytic_names:
+            m = Analytic(name=name)
+            m.save()
+
+        network_names = [
+            'Transmits user data to multiple ad networks',
+            'Transmits information to Facebook ad network',
+            'Transmits information to Google ad network',
+            'Transmits information to Adobe ad network',
+            'Transmits information to Yahoo! ad network',
+            'Transmits information to Salesforce platform',
+            'Transmits information to Twitter platform',
+            'Transmits information to Amazon ad network',
+            'Transmits information to Microsoft ad network',
+        ]
+
+        for name in network_names:
+            m = Network(name=name)
+            m.save()
