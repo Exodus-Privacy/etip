d3a39b9fdf93446df4dcdd2b966449d8e6aadc42
simpnu <simon@pruneau.info>
2019-10-26 14:52:27 +0200

    Add serializer for TrackerCategory and detailed test for API

--- etip/restful_api/tests.py
+++ etip/restful_api/tests.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
-from trackers.models import Tracker
+from trackers.models import Tracker, TrackerCategory
 
 
 class RestfulApiGetAllTrackersTests(APITestCase):
@@ -23,6 +23,39 @@ class RestfulApiGetAllTrackersTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 0)
+
+    def test_get_trackers_with_details(self):
+        self.force_autentication()
+
+        tracker = Tracker.objects.create(
+            name='tracker1',
+            description='description of the tracker',
+            code_signature='com.tracker1.ads',
+            network_signature='tracker1.com',
+            website='https://tracker1.com',
+            is_in_exodus=True,
+        )
+
+        category1 = TrackerCategory.objects.create(name='Ads')
+        category2 = TrackerCategory.objects.create(name='Location')
+        tracker.category.add(category1)
+        tracker.category.add(category2)
+
+        expected_tracker = [{
+            'name': 'tracker1',
+            'description': 'description of the tracker',
+            'code_signature': 'com.tracker1.ads',
+            'network_signature': 'tracker1.com',
+            'website': 'https://tracker1.com',
+            'is_in_exodus': True,
+            'category': [{'name': 'Ads'}, {'name': 'Location'}]
+        }]
+
+        response = self.client.get(self.TRACKERS_PATH)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.json(), expected_tracker)
 
     def test_get_trackers_when_2(self):
         self.force_autentication()
