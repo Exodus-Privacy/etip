48e75a0f895742a02cf1f5c0ac7d8de192099a3e
simpnu <simon@pruneau.info>
2019-10-26 14:52:27 +0200

    Add serializer for TrackerCategory and detailed test for API

--- etip/restful_api/serializers.py
+++ etip/restful_api/serializers.py
@@ -1,8 +1,16 @@
-from trackers.models import Tracker
+from trackers.models import Tracker, TrackerCategory
 from rest_framework import serializers
 
 
+class TrackerCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrackerCategory
+        fields = ['name']
+
+
 class TrackerSerializer(serializers.ModelSerializer):
+    category = TrackerCategorySerializer(read_only=True, many=True)
+
     class Meta:
         model = Tracker
         fields = [
