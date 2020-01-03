6019f5236032939aae4816bbb2cc6c0e3ddb9def
simpnu <simon@pruneau.info>
2020-01-03 13:25:03 +0100

    Use new django URL format

--- etip/trackers/urls.py
+++ etip/trackers/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'trackers'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^trackers/export$', views.export_tracker_list, name='export')
+    path('', views.index, name='index'),
+    path('trackers/export', views.export_tracker_list, name='export')
 ]
