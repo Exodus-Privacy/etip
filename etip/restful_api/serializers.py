b369a2b2d76d560e6a6d2e5e5ce02b8dca8d8d33
simpnu <simon@pruneau.info>
2020-04-21 19:00:34 +0200

    Add UUID to ETIP API

--- etip/restful_api/serializers.py
+++ etip/restful_api/serializers.py
@@ -14,6 +14,7 @@ class TrackerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tracker
         fields = [
+          'id',
           'name',
           'category',
           'description',
