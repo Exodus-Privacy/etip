883818b4deba8f43c62bc5bd1f257bbe3bc41945
Pnu <6069449+pnu-s@users.noreply.github.com>
2022-05-14 21:53:27 +0200

    chore: Upgrade to django 3.2 (#145)

--- etip/etip/settings/base.py
+++ etip/etip/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Application definition
@@ -73,6 +74,7 @@ WSGI_APPLICATION = 'etip.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -119,3 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_URL = '/admin/login/'
+
+# See https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
