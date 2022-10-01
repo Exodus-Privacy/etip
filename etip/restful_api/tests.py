4fe7c60813fef445149ffd2e795087501ab245c9
pnu <simon@pruneau.info>
2022-10-01 14:11:23 +0200

    feat: Allow public access to /api/trackers

--- etip/restful_api/tests.py
+++ etip/restful_api/tests.py
@@ -1,6 +1,5 @@
 from datetime import date
 
-from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -12,24 +11,13 @@ class RestfulApiGetAllTrackersTests(APITestCase):
     TRACKERS_PATH = '/api/trackers/'
     TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'  # ISO8601
 
-    def force_autentication(self):
-        user = User.objects.create_user('username', 'Pas$w0rd')
-        self.client.force_authenticate(user)
-
-    def test_get_unauthorized_when_no_auth(self):
-        response = self.client.get(self.TRACKERS_PATH)
-
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
     def test_get_empty_when_no_tracker(self):
-        self.force_autentication()
         response = self.client.get(self.TRACKERS_PATH)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 0)
 
     def test_get_trackers_with_details_without_documentation(self):
-        self.force_autentication()
 
         tracker = Tracker.objects.create(
             name='tracker1',
@@ -86,7 +74,6 @@ class RestfulApiGetAllTrackersTests(APITestCase):
         self.assertEqual(response.json(), expected_tracker)
 
     def test_get_trackers_with_details(self):
-        self.force_autentication()
 
         tracker = Tracker.objects.create(
             name='tracker1',
@@ -161,7 +148,6 @@ class RestfulApiGetAllTrackersTests(APITestCase):
         self.assertEqual(response.json(), expected_tracker)
 
     def test_get_trackers_when_2(self):
-        self.force_autentication()
 
         Tracker.objects.create(name='toto')
         Tracker.objects.create(name='toto2')
@@ -172,21 +158,18 @@ class RestfulApiGetAllTrackersTests(APITestCase):
         self.assertEqual(len(response.data), 2)
 
     def test_post_is_not_allowed(self):
-        self.force_autentication()
         response = self.client.post(self.TRACKERS_PATH)
 
         self.assertEqual(response.status_code,
                          status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_put_is_not_allowed(self):
-        self.force_autentication()
         response = self.client.put(self.TRACKERS_PATH)
 
         self.assertEqual(response.status_code,
                          status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_delete_is_not_allowed(self):
-        self.force_autentication()
         response = self.client.delete(self.TRACKERS_PATH)
 
         self.assertEqual(response.status_code,
--- etip/restful_api/views.py
+++ etip/restful_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.decorators import authentication_classes, permission_classes
 
 from restful_api.serializers import TrackerSerializer
 from trackers.models import Tracker
 
 
+@authentication_classes(())
+@permission_classes(())
 class TrackerViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows trackers to be viewed or edited.
