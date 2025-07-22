066974862939881c5dc0949d5d04fe440fa1c343
pnu <6069449+pnu-s@users.noreply.github.com>
2025-07-22 13:13:18 +0200

    chore: Bump django from 3.2.25 to 4.2.16 (#192)
    * chore: Bump django from 3.2.25 to 4.2.16
    
    * test: bump python

--- etip/etip/urls.py
+++ etip/etip/urls.py
@@ -13,9 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
