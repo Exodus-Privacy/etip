a28a9d7356eb946ef8b78c4e93270a7da09d61da
Doctor <codimp@exodus-privacy.eu.org>
2025-07-31 23:44:53 +0200

    settings: add static files variables

--- etip/etip/settings/docker.py
+++ etip/etip/settings/docker.py
@@ -3,3 +3,7 @@ from .base import *
 
 SECRET_KEY = 'h6a8)tm@#emr=o%866(ek)p%xlnd9_xmd8y_*2$5+@o7+)(tf#'
 DEBUG = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = f'{ROOT_DIR}/staticfiles/'
+STATICFILES_DIRS = [f'{BASE_DIR}/static']
